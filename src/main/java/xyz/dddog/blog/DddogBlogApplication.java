7f704c16ab31c703486d7be8a4fc139eb6d5eaff
dddog <azskw1101@gmail.com>
2017-02-13 14:52:41 +0900

    init commit

---
+++ src/main/java/xyz/dddog/blog/DddogBlogApplication.java
@@ -0,0 +1,12 @@
+package xyz.dddog.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddogBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddogBlogApplication.class, args);
+	}
+}
---
+++ src/main/java/xyz/dddog/blog/presentation/controller/web/HelloController.java
@@ -0,0 +1,15 @@
+package xyz.dddog.blog.presentation.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping({"/","/hello"})
+	public String hello(Model model) {
+		model.addAttribute("name", "dddog");
+		return "hello";
+	}
+}
